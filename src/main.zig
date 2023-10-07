bd10fc5c9061e190322f40574543db4084019961
grayhatter <_@gr.ht>
2023-10-06 21:37:32 -0700

    debug uwsgi proto

--- src/main.zig
+++ src/main.zig
@@ -96,10 +96,71 @@ fn serveHttp(srv: *Server, a: std.mem.Allocator) !void {
     }
 }
 
+fn uwsgiHeader(a: std.mem.Allocator, acpt: std.net.StreamServer.Connection) ![][]u8 {
+    var list = std.ArrayList([]u8).init(a);
+
+    // TODO packed
+    const uHEADER = extern struct {
+        mod1: u8 = 0,
+        size: u16 = 0,
+        mod2: u8 = 0,
+    };
+
+    var uwsgi_header: *uHEADER = undefined;
+    var h_buf: [4]u8 align(@alignOf(uHEADER)) = .{ 0, 0, 0, 0 };
+    _ = try acpt.stream.read(&h_buf);
+    uwsgi_header = @as(*uHEADER, @ptrCast(&h_buf));
+    std.log.info("header {}", .{uwsgi_header});
+    var rsize = [1]u8{0};
+    var rcount: usize = 0;
+
+    var b: [8192]u8 = undefined;
+    header: while (true) {
+        rcount = 0;
+        while (try acpt.stream.read(&rsize) != 0) {
+            if (rsize[0] == 0) {
+                if (rcount == 0) {
+                    std.log.info("data [empty]", .{});
+                    continue :header;
+                }
+                break;
+            }
+            rcount += rsize[0];
+        }
+        std.debug.assert(rcount > 0);
+        var buf = b[0..rcount];
+        var in = try acpt.stream.read(buf);
+        std.debug.assert(in == rcount);
+        std.log.info("data {any}", .{buf});
+        std.log.info("data {s}", .{buf});
+        if (in == 0 and rcount == 0) break;
+    }
+
+    return list.toOwnedSlice();
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{ .stack_trace_frames = 12 }){};
     defer _ = gpa.deinit();
     const a = gpa.allocator();
+
+    var usock = std.net.StreamServer.init(.{});
+    const FILE = "./srctree.sock";
+
+    var cwd = std.fs.cwd();
+    if (cwd.access(FILE, .{})) {
+        try cwd.deleteFile(FILE);
+    } else |_| {}
+
+    const uaddr = try std.net.Address.initUnix(FILE);
+    try usock.listen(uaddr);
+    std.log.info("Unix server listening\n", .{});
+    var acpt = try usock.accept();
+
+    _ = try uwsgiHeader(a, acpt);
+
+    acpt.stream.close();
+    usock.close();
 
     var srv = Server.init(a, .{ .reuse_address = true });
 
