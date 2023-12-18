f8485cc02d0fc6ead753b1f62d57559a14746109
Gregory Mullen <_@gr.ht>
2023-12-17 23:31:54 -0800

    delete unneeded function

--- src/types/journal.zig
+++ src/types/journal.zig
@@ -50,29 +50,6 @@ pub fn new() !Journal {
     return error.NotImplemnted;
 }
 
-pub fn findMTLSFingerprint(a: Allocator, fp: []const u8) !Journal {
-    if (fp.len != 40) return error.InvalidFingerprint;
-    var idir = try datad.openIterableDir(".", .{});
-    defer idir.close();
-    var itr = idir.iterate();
-    while (try itr.next()) |f| {
-        if (f.kind != .file) continue;
-        const file = try datad.openFile(f.name, .{});
-        errdefer file.close();
-        var ckb: [40]u8 = undefined;
-        var count = try file.read(&ckb);
-        if (count != 40) {
-            file.close();
-            continue;
-        }
-        if (std.mem.eql(u8, &ckb, fp)) {
-            try file.seekTo(0);
-            return try Journal.readFile(a, f.name, file);
-        }
-    }
-    return error.UserNotFound;
-}
-
 pub fn open(a: Allocator, username: []const u8) !Journal {
     for (username) |c| if (!std.ascii.isLower(c)) return error.InvalidUsername;
 
