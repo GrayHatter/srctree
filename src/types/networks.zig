146f64763341f115cb0ecf2e6848c6beafbf7be7
Gregory Mullen <_@gr.ht>
2023-12-28 22:56:20 -0800

    expand network struct

--- src/types/networks.zig
+++ src/types/networks.zig
@@ -3,6 +3,27 @@ const Allocator = std.mem.Allocator;
 
 pub const Networks = @This();
 
+const NET_VERSION: usize = 0;
+
+pub const Network = struct {
+    name: []u8,
+    location: []u8,
+    highlight: u8,
+
+    file: std.fs.File,
+
+    pub fn writeOut(self: Network) !void {
+        try self.file.seekTo(0);
+        const w = self.file.writer();
+        try w.writeIntNative(usize, NET_VERSION);
+        try w.writeAll(self.name);
+        try w.writeAll("\x00");
+        try w.writeAll(self.uri);
+        try w.writeAll("\x00");
+        try w.writeIntNative(u8, self.highlight);
+    }
+};
+
 var datad: std.fs.Dir = undefined;
 
 pub fn init(dir: []const u8) !void {
