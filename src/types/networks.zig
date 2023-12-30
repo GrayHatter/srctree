fa2f49dbf6562ca7b8ae5ad3fcc3958765258e24
Gregory Mullen <_@gr.ht>
2023-12-29 23:22:04 -0800

    add new() for network type

--- src/types/networks.zig
+++ src/types/networks.zig
@@ -35,3 +35,30 @@ pub fn init(dir: []const u8) !void {
 pub fn raze() void {
     datad.close();
 }
+
+pub fn validName(name: []const u8) bool {
+    for (name) |c| switch (c) {
+        'a'...'z' => continue,
+        else => return false,
+    } else return true;
+}
+
+/// TODO actually allow real URIs, and check if the network location exists :D
+pub fn validLocation(loc: []const u8) bool {
+    return validName(loc);
+}
+
+pub fn new(name: []const u8, loc: []const u8, hl: bool) !Network {
+    if (!validName(name) or !validLocation(loc)) return error.InvalidNetwork;
+    var buf: [2048]u8 = undefined;
+    const filename = try std.fmt.bufPrint(&buf, "{s}.network", .{name});
+    var file = try datad.createFile(filename, .{});
+    var nw = Network{
+        .name = name,
+        .location = loc,
+        .highlight = if (hl) 1 else 0,
+        .file = file,
+    };
+    try nw.writeOut();
+    return nw;
+}
