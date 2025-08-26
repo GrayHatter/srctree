8b0457f94d778724c48983c1c6e26bfd1973fbc6
Gregory Mullen <_@gr.ht>
2025-08-26 08:43:07 -0700

    new annoying bot

--- src/srctree.zig
+++ src/srctree.zig
@@ -11,6 +11,8 @@ pub const verse_routes = [_]Match{
         .{ .name = "barkrowler", .allow = false }, // selfish bot
         .{ .name = "ClaudeBot", .allow = false }, // aggressive, selfish
         .{ .name = "Amazonbot", .allow = false }, // aggressive, selfish
+        // Disallowed for being too aggressive, and substituting it's own crawl delay
+        .{ .name = "MJ12bot", .allow = false, .extra = "Crawl-Delay: 90\n" },
         .{
             .name = "ImagesiftBot",
             .allow = true,
