480e22b058058b31ca84d7acdeaa94324624ef1d
Gregory Mullen <_@gr.ht>
2025-09-29 23:58:46 -0700

    ban another seo crawler

--- src/srctree.zig
+++ src/srctree.zig
@@ -12,6 +12,7 @@ pub const verse_routes = [_]Match{
         .{ .name = "ClaudeBot", .allow = false }, // aggressive, selfish
         .{ .name = "Amazonbot", .allow = false }, // aggressive, selfish
         .{ .name = "AhrefsBot", .allow = false }, // selfish
+        .{ .name = "dotbot", .allow = false }, // selfish
         // Disallowed for being too aggressive, and substituting it's own crawl delay
         .{ .name = "MJ12bot", .allow = false, .extra = "Crawl-Delay: 90\n" },
         .{
