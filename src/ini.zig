4f80c173041f32ff663143e63c3070eed8af0e44
Gregory Mullen <_@gr.ht>
2026-01-15 09:12:34 -0800

    improve ini.zig
    red diff, best diff

--- src/ini.zig
+++ src/ini.zig
@@ -2,65 +2,51 @@ const Setting = struct {
     name: []const u8,
     val: []const u8,
 
-    fn pair(a: Allocator, str: []const u8) !Setting {
-        if (indexOfScalar(u8, str, '=')) |i| {
+    // str must outlive Setting
+    fn init(str: []const u8) Setting {
+        if (findScalar(u8, str, '=')) |i| {
             return .{
-                .name = try a.dupe(u8, trim(u8, str[0..i], " \n\t")),
-                .val = try a.dupe(u8, trim(u8, str[i + 1 ..], " \n\t")),
+                .name = trim(u8, str[0..i], " \n\t"),
+                .val = trim(u8, str[i + 1 ..], " \n\t"),
             };
         }
         unreachable;
     }
-
-    fn raze(s: Setting, a: Allocator) void {
-        a.free(s.name);
-        a.free(s.val);
-    }
 };
 
 pub const Namespace = struct {
-    name: []u8,
-    settings: []Setting,
+    name: []const u8,
+    settings: []const Setting,
     block: []const u8,
 
+    /// Name must outlive the Namespace
     pub fn init(a: Allocator, name: []const u8, itr: *ScalarIter) !Namespace {
-        var list: std.ArrayList(Setting) = .{};
-        errdefer {
-            for (list.items) |itm| itm.raze(a);
-            list.deinit(a);
-        }
+        var list: ArrayList(Setting) = .{};
+        errdefer list.deinit(a);
+
         const ns_start = itr.index.?;
-        const ns_block = itr.buffer[ns_start..];
 
-        while (itr.peek()) |peek| {
-            const line = trim(u8, peek, " \n\t");
-            if (line.len < 3 or line[0] == '#') {
-                _ = itr.next();
-                continue;
+        while (itr.next()) |next| {
+            const line = trim(u8, next, " \n\t");
+            if (line.len > 3 and line[0] != '#' and find(u8, line, "=") != null) {
+                try list.append(a, .init(line));
             }
-            if (line[0] == '[') break;
-            if (count(u8, line, "=") == 0) {
-                _ = itr.next();
-                continue;
+            if (itr.peek()) |peekW| {
+                const peek = trim(u8, peekW, " \t");
+                if (peek.len > 0 and peek[0] == '[') break;
             }
-            const pair = try Setting.pair(a, line);
-            try list.append(a, pair);
-            _ = itr.next();
         }
 
-        const ns_end = itr.index orelse itr.buffer.len;
         return .{
-            .name = try a.dupe(u8, name[1 .. name.len - 1]),
+            .name = name[1 .. name.len - 1],
             .settings = try list.toOwnedSlice(a),
-            .block = ns_block[0 .. ns_end - ns_start],
+            .block = itr.buffer[ns_start .. itr.index orelse itr.buffer.len],
         };
     }
 
     pub fn get(self: Namespace, name: []const u8) ?[]const u8 {
         for (self.settings) |st| {
-            if (eql(u8, name, st.name)) {
-                return st.val;
-            }
+            if (eql(u8, name, st.name)) return st.val;
         }
         return null;
     }
@@ -68,42 +54,37 @@ pub const Namespace = struct {
     pub fn getBool(self: Namespace, name: []const u8) ?bool {
         const set: []const u8 = self.get(name) orelse return null;
         if (set.len > 5) return null;
-        var buffer: [6]u8 = undefined;
-        const check = lowerString(buffer[0..], set);
-        if (eql(u8, check, "false") or eql(u8, check, "0") or eql(u8, check, "f")) {
+        if (eql(u8, set, "0") or eqlCaseless(set, "false") or eqlCaseless(set, "f")) {
             return false;
-        } else if (eql(u8, check, "true") or eql(u8, check, "1") or eql(u8, check, "t")) {
+        } else if (eql(u8, set, "1") or eqlCaseless(set, "true") or eqlCaseless(set, "t")) {
             return true;
         } else return null;
     }
 
-    pub fn raze(self: Namespace, a: Allocator) void {
-        a.free(self.name);
-        for (self.settings) |set| {
-            set.raze(a);
-        }
-        a.free(self.settings);
+    pub fn raze(ns: Namespace, a: Allocator) void {
+        a.free(ns.settings);
     }
 };
 
 pub const Any = Config(void);
 
-pub fn Config(B: anytype) type {
+pub fn Config(BaseT: type) type {
     return struct {
         config: Base,
         ctx: IniData,
 
-        pub const Base = B;
+        pub const Self = @This();
+
+        pub const Base = BaseT;
 
         pub const IniData = struct {
             ns: []Namespace,
             data: []const u8,
-            owned: ?[]const u8,
+            owned: ?[]const u8 = null,
 
             pub const empty: IniData = .{
                 .ns = &.{},
                 .data = &.{},
-                .owned = null,
             };
 
             pub fn filter(ctx: IniData, prefix: []const u8, index: usize) ?Namespace {
@@ -130,66 +111,44 @@ pub fn Config(B: anytype) type {
                 var namespace: T = undefined;
                 const ns = ctx.get(name) orelse return null;
                 inline for (@typeInfo(T).@"struct".fields) |s| {
-                    switch (s.type) {
-                        bool => {
-                            @field(namespace, s.name) = ns.getBool(s.name) orelse brk: {
-                                if (s.defaultValue()) |dv| {
-                                    break :brk dv;
-                                } else return error.SettingMissing;
-                            };
-                        },
-                        ?bool => {
-                            @field(namespace, s.name) = ns.getBool(s.name);
-                        },
-                        []const u8 => {
-                            @field(namespace, s.name) = ns.get(s.name) orelse return error.SettingMissing;
-                        },
-                        ?[]const u8 => {
-                            @field(namespace, s.name) = ns.get(s.name);
-                        },
+                    @field(namespace, s.name) = switch (s.type) {
+                        bool => ns.getBool(s.name) orelse s.defaultValue() orelse return error.SettingMissing,
+                        ?bool => ns.getBool(s.name),
+                        []const u8 => ns.get(s.name) orelse return error.SettingMissing,
+                        ?[]const u8 => ns.get(s.name),
                         else => @compileError("not implemented"),
-                    }
+                    };
                 }
                 return namespace;
             }
         };
 
-        pub const Self = @This();
-
         fn makeBase(self: IniData) !Base {
             if (Base == void) return {};
             var base: Base = undefined;
             inline for (@typeInfo(Base).@"struct".fields) |f| {
-                if (f.type == []const u8) continue; // Root variable not yet supported
-                switch (@typeInfo(f.type)) {
-                    .@"struct" => {
-                        @field(base, f.name) = try self.buildStruct(f.type, f.name) orelse return error.NamespaceMissing;
-                    },
-                    .optional => {
-                        @field(base, f.name) = self.buildStruct(@typeInfo(f.type).optional.child, f.name) catch null;
-                    },
+                if (f.type == []const u8) comptime unreachable; // Root variable not yet supported
+                @field(base, f.name) = switch (@typeInfo(f.type)) {
+                    .@"struct" => try self.buildStruct(f.type, f.name) orelse return error.NamespaceMissing,
+                    .optional => self.buildStruct(@typeInfo(f.type).optional.child, f.name) catch null,
                     else => @compileError("not implemented"),
-                }
+                };
             }
 
             return base;
         }
 
         pub fn raze(self: Self, a: Allocator) void {
-            for (self.ctx.ns) |ns| {
-                ns.raze(a);
-            }
+            for (self.ctx.ns) |ns| ns.raze(a);
             a.free(self.ctx.ns);
-            if (self.ctx.owned) |owned| {
-                a.free(owned);
-            }
+            if (self.ctx.owned) |owned| a.free(owned);
         }
 
         /// `data` must outlive returned object
         pub fn init(a: Allocator, data: []const u8) !Self {
             var itr = splitScalar(u8, data, '\n');
 
-            var list: std.ArrayList(Namespace) = .{};
+            var list: ArrayList(Namespace) = .{};
             errdefer {
                 for (list.items) |itm| itm.raze(a);
                 list.deinit(a);
@@ -200,7 +159,7 @@ pub fn Config(B: anytype) type {
                 if (line.len == 0) continue;
 
                 if (line[0] == '[' and line[line.len - 1] == ']') {
-                    try list.append(a, try Namespace.init(a, line, &itr));
+                    try list.append(a, try .init(a, line, &itr));
                 }
             }
 
@@ -217,12 +176,10 @@ pub fn Config(B: anytype) type {
         }
 
         pub fn fromFile(a: Allocator, io: std.Io, file: std.Io.File) !Self {
-            var w: Writer.Allocating = .init(a);
             var r_b: [2048]u8 = undefined;
             var reader = file.reader(io, &r_b);
-            _ = try reader.interface.stream(&w.writer, .limited(0x8000));
+            const data = try reader.interface.allocRemaining(a, .limited(0x8000));
 
-            const data = try w.toOwnedSlice();
             var self: Self = try init(a, data);
             self.ctx.owned = data;
             return self;
@@ -233,23 +190,17 @@ pub fn Config(B: anytype) type {
 test "default" {
     const a = std.testing.allocator;
 
-    const expected = Config(void){
+    const expected: Config(void) = .{
         .config = {},
         .ctx = .{
-            .ns = @constCast(&[1]Namespace{
-                Namespace{
-                    .name = @as([]u8, @constCast("one")),
-                    .settings = @constCast(&[1]Setting{
-                        .{
-                            .name = "left",
-                            .val = "right",
-                        },
-                    }),
-                    .block = @constCast("left = right"),
-                },
-            }),
+            .ns = @constCast(&[1]Namespace{.{
+                .name = "one",
+                .settings = @constCast(&[1]Setting{
+                    .{ .name = "left", .val = "right" },
+                }),
+                .block = @constCast("left = right"),
+            }}),
             .data = @constCast("[one]\nleft = right"),
-            .owned = null,
         },
     };
 
@@ -308,23 +259,18 @@ test "commented" {
         \\ # but not this
     ;
 
-    const expected = Config(void){
+    const expected: Config(void) = .{
         .config = {},
         .ctx = .{
-            .ns = @constCast(&[1]Namespace{
-                Namespace{
-                    .name = @as([]u8, @constCast("open")),
-                    .settings = @constCast(
-                        &[2]Setting{
-                            .{ .name = "left", .val = "right" },
-                            .{ .name = "this", .val = "works" },
-                        },
-                    ),
-                    .block = @constCast(vut[7..]),
-                },
-            }),
+            .ns = @constCast(&[1]Namespace{.{
+                .name = "open",
+                .settings = @constCast(&[2]Setting{
+                    .{ .name = "left", .val = "right" },
+                    .{ .name = "this", .val = "works" },
+                }),
+                .block = @constCast(vut[7..]),
+            }}),
             .data = @constCast(vut),
-            .owned = null,
         },
     };
 
@@ -335,14 +281,13 @@ test "commented" {
 }
 
 const std = @import("std");
-const eql = std.mem.eql;
-const trim = std.mem.trim;
-const count = std.mem.count;
-const startsWith = std.mem.startsWith;
-const lowerString = std.ascii.lowerString;
-const splitScalar = std.mem.splitScalar;
-const indexOfScalar = std.mem.indexOfScalar;
-const ScalarIter = std.mem.SplitIterator(u8, .scalar);
-
 const Allocator = std.mem.Allocator;
-const Writer = std.Io.Writer;
+const ArrayList = std.ArrayList;
+const ScalarIter = std.mem.SplitIterator(u8, .scalar);
+const eql = std.mem.eql;
+const eqlCaseless = std.ascii.eqlIgnoreCase;
+const find = std.mem.find;
+const findScalar = std.mem.findScalar;
+const splitScalar = std.mem.splitScalar;
+const startsWith = std.mem.startsWith;
+const trim = std.mem.trim;
