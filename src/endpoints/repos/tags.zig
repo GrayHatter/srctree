5a97a5ba96da53ae3b3d452e325ccf1fe1bd922b
Gregory Mullen <_@gr.ht>
2026-05-15 08:46:20 -0700

    replace old header in tags

--- src/endpoints/repos/tags.zig
+++ src/endpoints/repos/tags.zig
@@ -1,40 +1,46 @@
 const TagPage = PageData("repo-tags.html");
 
-pub fn list(frame: *Frame) Router.Error!void {
-    const rd = RouteData.init(frame.uri) orelse return error.Unrouteable;
+pub fn list(f: *Frame) Router.Error!void {
+    const rd = RouteData.init(f.uri) orelse return error.Unrouteable;
 
-    const vis: repos.Visibility.Select = if (frame.user) |_| .all else .public_only;
-    var repo = (repos.open(rd.name, vis, frame.io) catch return error.Unknown) orelse return error.InvalidURI;
-    repo.loadData(frame.alloc, frame.io) catch return error.Unknown;
-    defer repo.raze(frame.alloc, frame.io);
+    const vis: repos.Visibility.Select = if (f.user) |_| .all else .public_only;
+    var repo = (repos.open(rd.name, vis, f.io) catch return error.Unknown) orelse return error.InvalidURI;
+    repo.loadData(f.alloc, f.io) catch return error.Unknown;
+    defer repo.raze(f.alloc, f.io);
 
     var tags: std.ArrayList(Git.Tag) = .empty;
     for (repo.refs.keys(), repo.refs.values()) |tag_name, ref| switch (ref) {
-        .tag => |t| tags.append(frame.alloc, Git.Tag.fromObject(
-            repo.objects.load(t, frame.alloc, frame.io) catch continue,
-            frame.alloc.dupe(u8, tag_name) catch unreachable,
+        .tag => |t| tags.append(f.alloc, Git.Tag.fromObject(
+            repo.objects.load(t, f.alloc, f.io) catch continue,
+            f.alloc.dupe(u8, tag_name) catch unreachable,
         ) catch continue) catch unreachable,
         else => {},
     };
     std.sort.heap(Git.Tag, tags.items, {}, sort);
     var tstack: std.ArrayList(S.RepoTagsHtml.Tags) = .empty;
     for (tags.items) |tag| {
-        tstack.append(frame.alloc, .{ .name = .abx(tag.name) }) catch unreachable;
+        tstack.append(f.alloc, .{ .name = .abx(tag.name) }) catch unreachable;
     }
 
-    const upstream: ?S.RepoTagsHtml.Upstream = if (repo.findRemote("upstream")) |up| .{
-        .href = .abx(try allocPrint(frame.alloc, "{f}", .{std.fmt.alt(up, .formatLink)})),
-    } else null;
+    //const open_graph: S.OpenGraph = .{ .title = rd.name, .desc = page_desc orelse "" };
+    const repo_header: S.BaseRepoHeaderHtml = .{
+        .git_uri = .{ .host = .safe("srctree.gr.ht"), .repo_name = .abx(rd.name) },
+        .repo_name = .safe(rd.name),
+        .description = .abx(repo.description(f.alloc, f.io) catch ""),
+        .upstream = if (repo.findRemote("upstream")) |up| .{
+            .href = .abx(try allocPrint(f.alloc, "{f}", .{std.fmt.alt(up, .formatLink)})),
+        } else null,
+        .blame = null,
+    };
 
     var page = TagPage.init(.{
         .meta_head = .{ .open_graph = .{} },
-        .body_header = frame.response_data.get(S.BodyHeaderHtml).?.*,
-        .upstream = upstream,
+        .body_header = f.response_data.get(S.BodyHeaderHtml).?.*,
+        .repo_header = repo_header,
         .tags = tstack.items,
-        .repo_name = .abx(rd.name),
     });
 
-    try frame.sendPage(&page);
+    try f.sendPage(&page);
 }
 
 pub fn sort(_: void, l: Git.Tag, r: Git.Tag) bool {
